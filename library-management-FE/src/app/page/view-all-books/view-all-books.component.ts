cb1dc1b6e34b466013227558d915557890844ce0
Dinuk1994 <ddt94119@gmail.com>
2024-02-08 15:14:06 +0530

    adding data to the table

--- library-management-FE/src/app/page/view-all-books/view-all-books.component.ts
+++ library-management-FE/src/app/page/view-all-books/view-all-books.component.ts
@@ -1,18 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient,HttpClientModule } from '@angular/common/http';
-import { Appmodule } from '../../module';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-view-all-books',
   standalone: true,
-  imports: [HttpClientModule,Appmodule],
+  imports: [HttpClientModule,FormsModule,CommonModule],
   templateUrl: './view-all-books.component.html',
   styleUrl: './view-all-books.component.css'
 })
 export class ViewAllBooksComponent implements OnInit{
 
   private http;
-  private bookList: any = {}
+  public bookList: any = {}
 
   constructor(private httpClient: HttpClient) {
     this.http = httpClient
